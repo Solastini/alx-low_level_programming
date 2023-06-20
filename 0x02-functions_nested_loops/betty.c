cc04f9c6a3b17eae0f8147780f6a892169dc2eea
Sol <129546934+Solastini@users.noreply.github.com>
2023-06-20 13:38:14 -0800

    Update betty.c

--- 0x02-functions_nested_loops/betty.c
+++ 0x02-functions_nested_loops/betty.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - this is the main function
- * description - the starting point of the program
- */
-void main(void)
-{
-	int test = 5;
-
-	printf("xd\n", test);
-}
